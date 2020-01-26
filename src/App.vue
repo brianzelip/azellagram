fa781ad0037b716294d0d8988c19821e7fb99d60
Brian Zelip <bzelip@protonmail.com>
2020-01-26 09:02:13 -0500

    App.vue: Update template

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template lang="pug">
   main#app
-    h1 hello world!
+    h1 azellagram
+    h2.h3 Prepare raw Azellaz product photos for production
+    form.sm-col-6.mx-auto
+      TheFileInput
     a(href="https://github.com/brianzelip/azellagram") source
 </template>
 
 <script>
+import TheFileInput from './components/TheFileInput.vue';
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    TheFileInput
+  }
 };
 </script>
 
 <style scoped>
 main {
   text-align: center;
+}
+img {
+  width: 100px;
 }
 </style>
