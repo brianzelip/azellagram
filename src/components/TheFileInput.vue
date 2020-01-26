bc0c1f69adfb6cc8e3317fbc3977acd3e6c1bf04
Brian Zelip <bzelip@protonmail.com>
2020-01-26 16:28:21 -0500

    TheFileInput.vue: Send file select data to parent

--- src/components/TheFileInput.vue
+++ src/components/TheFileInput.vue
@@ -1,11 +1,31 @@
 <template lang="pug">
   div
     label(for="file-selector") Select image(s)
-    input.block.col-12.mb1.field(type="file" multiple id="file-selector")
+    input.block.col-12.mb1.field(type="file" multiple id="file-selector" v-on:change="updateFiles")
+    p Files: {{ files }}
 </template>
 
 <script>
 export default {
-  name: 'TheFileInput'
+  name: 'TheFileInput',
+  data() {
+    return {
+      files: []
+    };
+  },
+  methods: {
+    updateFiles(e) {
+      const fList = e.target.files;
+      const files = [];
+
+      fList.forEach(f => {
+        files.push(f.name);
+      });
+
+      this.$set(this, 'files', files);
+
+      this.$emit('file-input', this.files);
+    }
+  }
 };
 </script>
