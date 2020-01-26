bcd8097437167e9ab7313cb385fe28f6ecdb1563
Brian Zelip <bzelip@protonmail.com>
2020-01-26 09:03:02 -0500

    src/components/TheFileInput.vue: Create component

---
+++ src/components/TheFileInput.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  div
+    label(for="file-selector") Select image(s)
+    input.block.col-12.mb1.field(type="file" multiple id="file-selector")
+</template>
+
+<script>
+export default {
+  name: 'TheFileInput'
+};
+</script>
