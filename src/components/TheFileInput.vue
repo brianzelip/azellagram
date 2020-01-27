fd952e9930491bf8731fe544e0ef415e141ff2fd
Brian Zelip <bzelip@protonmail.com>
2020-01-27 06:06:55 -0500

    TheFileInput.vue: Clean up, send FileList to parent

--- src/components/TheFileInput.vue
+++ src/components/TheFileInput.vue
@@ -1,30 +1,15 @@
 <template lang="pug">
   div
-    label(for="file-selector") Select image(s)
-    input.block.col-12.mb1.field(type="file" multiple id="file-selector" v-on:change="updateFiles")
-    p Files: {{ files }}
+    label(for="file-selector") Select image file(s)
+    input.block.col-12.mb1.field(type="file" multiple id="file-selector" v-on:change="fileSelect")
 </template>
 
 <script>
 export default {
-  name: 'TheFileInput',
-  data() {
-    return {
-      files: []
-    };
-  },
+  name: "TheFileInput",
   methods: {
-    updateFiles(e) {
-      const fList = e.target.files;
-      const files = [];
-
-      fList.forEach(f => {
-        files.push(f.name);
-      });
-
-      this.$set(this, 'files', files);
-
-      this.$emit('file-input', this.files);
+    fileSelect(e) {
+      this.$emit("file-select", e.target.files);
     }
   }
 };
